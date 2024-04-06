c161316a41ac1b5bb1574cfe76823aab7d97a005
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-04-06 13:08:13 +0200

    Implementing Error and Success Message

--- apps/Forkify/dist/index.e37f48ea.js
+++ apps/Forkify/dist/index.e37f48ea.js
@@ -604,7 +604,7 @@ const controlRecipes = async function() {
         // 2) Rendering recipe
         (0, _recipeViewJsDefault.default).render(_modelJs.state.recipe);
     } catch (err) {
-        alert(err);
+        (0, _recipeViewJsDefault.default).renderError();
     }
 };
 const init = function() {
@@ -2489,6 +2489,7 @@ const loadRecipe = async function(id) {
     } catch (err) {
         //Temporary error handling
         console.error(`${err} \u{1F4A5} \u{1F4A5} \u{1F4A5} \u{1F4A5}`);
+        throw err;
     }
 };
 
@@ -2536,6 +2537,8 @@ var _fractional = require("fractional");
 class RecipeView {
     #parentElement = document.querySelector(".recipe");
     #data;
+    #errorMessage = "We could not find that recipe. Please try another one!";
+    #message = ";";
     render(data) {
         this.#data = data;
         const markup = this.#generateMarkup();
@@ -2545,7 +2548,7 @@ class RecipeView {
     #clear() {
         this.#parentElement.innerHTML = "";
     }
-    renderSpinner = function() {
+    renderSpinner() {
         const markup = `
     <div class="spinner">
     <svg>
@@ -2553,9 +2556,37 @@ class RecipeView {
     </svg>
   </div>
     `;
-        this.#parentElement.innerHTML = "";
+        this.#clear();
         this.#parentElement.insertAdjacentHTML("afterbegin", markup);
-    };
+    }
+    renderError(message = this.#errorMessage) {
+        const markup = `
+    <div class="error">
+            <div>
+              <svg>
+                <use href="${(0, _iconsSvgDefault.default)}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
+    renderMessage(message = this.#message) {
+        const markup = `
+    <div class="message">
+            <div>
+              <svg>
+                <use href="${(0, _iconsSvgDefault.default)}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
     addHandlerRender(handler) {
         [
             "hashchange",
