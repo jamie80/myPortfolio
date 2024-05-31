da19d901147eae22ce993614def288bf0fe84ea4
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-05-31 20:19:42 +0200

    Load and display High Scores from Local Storage

--- websites/english_street/quiz/end.js
+++ websites/english_street/quiz/end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORE = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -10,6 +14,16 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = (e) => {
-  console.log("clicked");
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("./");
 };
