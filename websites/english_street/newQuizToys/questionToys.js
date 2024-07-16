80506eccc26fef37d1a6a96c499cd447df25140a
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-07-16 11:10:52 +0200

    Add newQuizToys

---
+++ websites/english_street/newQuizToys/questionToys.js
@@ -0,0 +1,63 @@
+//Array of objects
+const quiz = [
+  {
+    q: "Jak powiedzieć po angielsku BALON?",
+    options: ["car", "balloon", "dinosaur", "doll"],
+    answer: 1,
+    img: "./img/balloon.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku POCIĄG?",
+    options: ["teddy bear", "train", "rocket", "plane"],
+    answer: 1,
+    img: "./img/train.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku SAMOCHÓD?",
+    options: ["plane", "kite", "doll", "car"],
+    answer: 3,
+    img: "./img/car.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku MIŚ?",
+    options: ["doll", "teddy bear", "dinosaur", "car"],
+    answer: 1,
+    img: "./img/teddy_bear.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku RAKIETA?",
+    options: ["teddy bear", "train", "rocket", "balloon"],
+    answer: 2,
+    img: "./img/rocket.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku DINOZAUR?",
+    options: ["robot", "doll", "dinosaur", "train"],
+    answer: 2,
+    img: "./img/dinosaur.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku LALKA?",
+    options: ["doll", "kite", "plane", "robot"],
+    answer: 0,
+    img: "./img/doll.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku LATAWIEC?",
+    options: ["plane", "balloon", "car", "kite"],
+    answer: 3,
+    img: "./img/kite.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku SAMOLOT?",
+    options: ["dinosaur", "plane", "doll", "kite"],
+    answer: 1,
+    img: "./img/plane.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku ROBOT?",
+    options: ["teddy bear", "rocket", "robot", "train"],
+    answer: 2,
+    img: "./img/robot.jpg",
+  },
+];
