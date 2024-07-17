2acc293226916bd0fab7f94aed21602d4cfda4bb
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-07-17 09:26:15 +0200

    Add newQuizFruit and newQuizTransport

---
+++ websites/english_street/newQuizFruit/questionFruit.js
@@ -0,0 +1,63 @@
+//Array of objects
+const quiz = [
+  {
+    q: "Jak powiedzieć po angielsku JABŁKO?",
+    options: ["banana", "apple", "cherry", "lemon"],
+    answer: 1,
+    img: "./img/apple.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku ARBUZ?",
+    options: ["blueberry", "watermelon", "cherry", "lemon"],
+    answer: 1,
+    img: "./img/watermelon.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku BANAN?",
+    options: ["apple", "strawberry", "pineapple", "banana"],
+    answer: 3,
+    img: "./img/banana.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku TRUSKAWKA?",
+    options: ["peach", "strawberry", "orange", "lemon"],
+    answer: 1,
+    img: "./img/strawberry.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku JAGODA?",
+    options: ["apple", "cherry", "blueberry", "banana"],
+    answer: 2,
+    img: "./img/blueberry.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku ANANAS?",
+    options: ["lemon", "apple", "pineapple", "watermelon"],
+    answer: 2,
+    img: "./img/pineapple.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku WIŚNIA?",
+    options: ["cherry", "orange", "pineapple", "blueberry"],
+    answer: 0,
+    img: "./img/cherry.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku POMARAŃCZA?",
+    options: ["strawberry", "lemon", "peach", "orange"],
+    answer: 3,
+    img: "./img/orange.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku CYTRYNA?",
+    options: ["cherry", "lemon", "banana", "blueberry"],
+    answer: 1,
+    img: "./img/lemon.jpg",
+  },
+  {
+    q: "Jak powiedzieć po angielsku BRZOSKWINIA?",
+    options: ["lemon", "apple", "peach", "strawberry"],
+    answer: 2,
+    img: "./img/peach.jpg",
+  },
+];
