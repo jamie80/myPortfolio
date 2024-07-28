c6c44dda5840563f84380a4a7fd81bb2ab79cfa3
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-07-28 20:49:34 +0200

    Add hangman (first commit)

---
+++ websites/angielska_ulica/hangman/scripts/script.js
@@ -0,0 +1,52 @@
+const hangmanImage = document.querySelector(".hangman-box img");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+
+let currentWord,
+  wrongGuessCount = 0;
+const maxGuesses = 6;
+
+const getRandomWord = () => {
+  // Selecting a random word and hint from the wordList
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word;
+  console.log(word);
+  document.querySelector(".hint-text b").innerText = hint;
+  wordDisplay.innerHTML = word
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+};
+
+const initGame = (button, clickedLetter) => {
+  // Checking if clickedLetter is existing on the currentWord
+  if (currentWord.includes(clickedLetter)) {
+    // Showing all correct letters on the word display
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    });
+  } else {
+    // If clicked letter doesn't exist then update the wrongGuessCount and hangman image
+    wrongGuessCount++;
+    hangmanImage.src = `./images/hangman-${wrongGuessCount}.svg`;
+  }
+
+  button.disabled = true;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+};
+
+// Creating keyboard buttons and adding event listeners
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i))
+  );
+}
+
+getRandomWord();
---
+++ websites/angielska_ulica/hangman/scripts/word-list.js
@@ -0,0 +1,46 @@
+const wordList = [
+  {
+    word: "cat",
+    hint: "Ulubieniec wielu dzieci.",
+  },
+  {
+    word: "crocodile",
+    hint: "Niebezpieczne zwierzę.",
+  },
+  {
+    word: "parrot",
+    hint: "Kolorowe zwierzę.",
+  },
+  {
+    word: "dog",
+    hint: "Przyjaciel człowieka.",
+  },
+  {
+    word: "mouse",
+    hint: "Małe zwierzę.",
+  },
+  {
+    word: "bird",
+    hint: "Może mieć różne kolory.",
+  },
+  {
+    word: "bird",
+    hint: "Może mieć różne kolory.",
+  },
+  {
+    word: "hedgehog",
+    hint: "Można się o niego pokłóć.",
+  },
+  {
+    word: "bee",
+    hint: "Potrafi latać.",
+  },
+  {
+    word: "chick",
+    hint: "Związany ze świętami.",
+  },
+  {
+    word: "rabbit",
+    hint: "Ma dobry słuch.",
+  },
+];
