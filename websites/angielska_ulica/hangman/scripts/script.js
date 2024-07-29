4c29d85947fe5dbf23f98fa6f4404760d1e12ca5
Wojciech Paszkiewicz <wojciechpaszkiewicz@op.pl>
2024-07-29 11:31:32 +0200

    Add hangman

--- websites/angielska_ulica/hangman/scripts/script.js
+++ websites/angielska_ulica/hangman/scripts/script.js
@@ -2,21 +2,51 @@ const hangmanImage = document.querySelector(".hangman-box img");
 const wordDisplay = document.querySelector(".word-display");
 const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
 
-let currentWord,
-  wrongGuessCount = 0;
+let currentWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
+
+const resetGame = () => {
+  // Resseting all game variables and UI elements
+  correctLetters = [];
+  wrongGuessCount = 0;
+  hangmanImage.src = `./images/hangman-${wrongGuessCount}.svg`;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  keyboardDiv
+    .querySelectorAll("button")
+    .forEach((btn) => (btn.disabled = false));
+  wordDisplay.innerHTML = currentWord
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+  gameModal.classList.remove("show");
+};
 
 const getRandomWord = () => {
   // Selecting a random word and hint from the wordList
   const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
   currentWord = word;
-  console.log(word);
   document.querySelector(".hint-text b").innerText = hint;
-  wordDisplay.innerHTML = word
-    .split("")
-    .map(() => `<li class="letter"></li>`)
-    .join("");
+  resetGame();
+};
+
+const gameOver = (isVictory) => {
+  // After 600ms of game complete... showing modal with relevant details
+  setTimeout(() => {
+    const modalText = isVictory ? `Odgadnięty wyraz to:` : `Poprawny wyraz to:`;
+    gameModal.querySelector("img").src = `./images/${
+      isVictory ? "victory" : "lost"
+    }.gif`;
+    gameModal.querySelector("h4").innerText = `${
+      isVictory ? "Gratulacje!" : "Koniec gry!"
+    }`;
+    gameModal.querySelector(
+      "p"
+    ).innerHTML = `${modalText} <b>${currentWord}</b>`;
+    gameModal.classList.add("show");
+  }, 300);
 };
 
 const initGame = (button, clickedLetter) => {
@@ -25,6 +55,7 @@ const initGame = (button, clickedLetter) => {
     // Showing all correct letters on the word display
     [...currentWord].forEach((letter, index) => {
       if (letter === clickedLetter) {
+        correctLetters.push(letter);
         wordDisplay.querySelectorAll("li")[index].innerText = letter;
         wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
       }
@@ -37,6 +68,10 @@ const initGame = (button, clickedLetter) => {
 
   button.disabled = true;
   guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+  // Calling gameOver() function if any of these condition meets.
+  if (wrongGuessCount === maxGuesses) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
 // Creating keyboard buttons and adding event listeners
@@ -50,3 +85,4 @@ for (let i = 97; i <= 122; i++) {
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
--- websites/angielska_ulica/hangman/scripts/word-list.js
+++ websites/angielska_ulica/hangman/scripts/word-list.js
@@ -1,46 +1,238 @@
 const wordList = [
   {
     word: "cat",
-    hint: "Ulubieniec wielu dzieci.",
+    hint: "Zwierzę",
   },
   {
     word: "crocodile",
-    hint: "Niebezpieczne zwierzę.",
+    hint: "Zwierzę.",
   },
   {
     word: "parrot",
-    hint: "Kolorowe zwierzę.",
+    hint: "Zwierzę.",
   },
   {
     word: "dog",
-    hint: "Przyjaciel człowieka.",
+    hint: "Zwierzę.",
   },
   {
     word: "mouse",
-    hint: "Małe zwierzę.",
+    hint: "Zwierzę.",
   },
   {
     word: "bird",
-    hint: "Może mieć różne kolory.",
-  },
-  {
-    word: "bird",
-    hint: "Może mieć różne kolory.",
+    hint: "Zwierzę.",
   },
   {
     word: "hedgehog",
-    hint: "Można się o niego pokłóć.",
+    hint: "Zwierzę.",
   },
   {
     word: "bee",
-    hint: "Potrafi latać.",
+    hint: "Zwierzę.",
   },
   {
     word: "chick",
-    hint: "Związany ze świętami.",
+    hint: "Zwierzę.",
   },
   {
     word: "rabbit",
-    hint: "Ma dobry słuch.",
+    hint: "Zwierzę.",
+  },
+  {
+    word: "train",
+    hint: "Zabawka.",
+  },
+  {
+    word: "kite",
+    hint: "Zabawka.",
+  },
+  {
+    word: "car",
+    hint: "Zabawka.",
+  },
+  {
+    word: "doll",
+    hint: "Zabawka.",
+  },
+  {
+    word: "teddy bear",
+    hint: "Zabawka.",
+  },
+  {
+    word: "robot",
+    hint: "Zabawka.",
+  },
+  {
+    word: "rocket",
+    hint: "Zabawka.",
+  },
+  {
+    word: "dinosaur",
+    hint: "Zabawka.",
+  },
+  {
+    word: "plane",
+    hint: "Zabawka.",
+  },
+  {
+    word: "balloon",
+    hint: "Zabawka.",
+  },
+  {
+    word: "butter",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "fish",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "bread",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "meat",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "pizza",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "sandwich",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "egg",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "chips",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "pasta",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "blueberry",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "banana",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "orange",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "strawberry",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "milk",
+    hint: "Jedzenie lub picie.",
+  },
+  {
+    word: "watermelon",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "cherry",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "lemon",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "peach",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "pineapple",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "apple",
+    hint: "Jedzenie.",
+  },
+  {
+    word: "bike",
+    hint: "Transport.",
+  },
+  {
+    word: "helicopter",
+    hint: "Transport.",
+  },
+  {
+    word: "motorbike",
+    hint: "Transport.",
+  },
+  {
+    word: "ship",
+    hint: "Transport.",
+  },
+  {
+    word: "van",
+    hint: "Transport.",
+  },
+  {
+    word: "car",
+    hint: "Transport.",
+  },
+  {
+    word: "scooter",
+    hint: "Transport.",
+  },
+  {
+    word: "train",
+    hint: "Transport.",
+  },
+  {
+    word: "bus",
+    hint: "Transport.",
+  },
+  {
+    word: "skateboard",
+    hint: "Transport.",
+  },
+  {
+    word: "dad",
+    hint: "Rodzina.",
+  },
+  {
+    word: "uncle",
+    hint: "Rodzina.",
+  },
+  {
+    word: "mum",
+    hint: "Rodzina.",
+  },
+  {
+    word: "sister",
+    hint: "Rodzina.",
+  },
+  {
+    word: "cousin",
+    hint: "Rodzina.",
+  },
+  {
+    word: "grandpa",
+    hint: "Rodzina.",
+  },
+  {
+    word: "brother",
+    hint: "Rodzina.",
+  },
+  {
+    word: "grandma",
+    hint: "Rodzina.",
+  },
+  {
+    word: "aunt",
+    hint: "Rodzina.",
   },
 ];
